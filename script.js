40ed3dcc7c34e1d38406bcaf17435d74d3faf786
radoslawstepniewski <radoslawstepniewski89@gmail.com>
2018-08-03 22:08:14 +0200

    added ball in center play field

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const ctx = canvas.getContext('2d');
 const cw = canvas.width = "1000";
 const ch = canvas.height = "500";
 
+const ballSize = 20; 
+
+ctx.fillRect(cw/2, ch/2 ,ballSize, ballSize)
+
+table = ()=>{
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cw, ch);
+}
+table();
+ctx.fillStyle = "red"
+ctx.fillRect(cw / 2 - ballSize /2, ch / 2 - ballSize /2, ballSize , ballSize)
+
+
+
+
