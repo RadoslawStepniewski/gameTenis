2dbff53bd40485fe76098cb1c059896123a2c90e
radoslawstepniewski <radoslawstepniewski89@gmail.com>
2018-08-04 15:38:58 +0200

    created player and ai and added to the playing field

--- script.js
+++ script.js
@@ -6,16 +6,41 @@ const ch = canvas.height = "500";
 
 const ballSize = 20; 
 
-ctx.fillRect(cw/2, ch/2 ,ballSize, ballSize)
+let ballX = cw / 2 - ballSize / 2
+let ballY = ch / 2 - ballSize / 2
+
+const paddleHeight = 100;
+const paddleWidth = 20;
+
+const playerX = 70;
+const aiX = 910;
+
+let playerY = ch / 2;
+let aiY = ch /2;
+
+player = () =>{
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(playerX,playerY - paddleHeight /2, paddleWidth, paddleHeight)
+}
+
+ai = () => {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(aiX, aiY - paddleHeight /2  , paddleWidth, paddleHeight)
+}
+
+ball =() =>{
+    ctx.fillStyle = "red"
+    ctx.fillRect(ballX, ballY, ballSize, ballSize)
+
+}
+
 
 table = ()=>{
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, cw, ch);
 }
 table();
-ctx.fillStyle = "red"
-ctx.fillRect(cw / 2 - ballSize /2, ch / 2 - ballSize /2, ballSize , ballSize)
-
-
-
+ball();
+player()
+ai();
 
